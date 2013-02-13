c4b7945de337638d465c6e6481100d44764b0945
Surim Kim <kimsuelim@gmail.com>
2013-02-13 17:30:12 +0900

    Initilize rstudy app

---
+++ dev/rstudy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rstudy::Application.config.secret_token = 'b8d688c640611088043debb481fb5dad6772914fccef7a382aaea1265e2d3b986ad9f7508b6b132da8ce9ae6cd1d5dc3c465af6761a8d0ff1f70c81d5acd33f2'
